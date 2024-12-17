c36180b045447a7afd6fd2093fa67cebedf17b17
unknown <cafeinai311@gmail.com>
2024-12-17 23:17:36 +0700

    init

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -9,5 +9,7 @@
 <body>
   <h1>Alloo Celpia Isnaeni, ciwi aku yang lagi skripsi</h1>
   <h2>semangat ngerjain skripsinya yaa ♡♡♡</h2>
+  <img src="https://i.pinimg.com/236x/e7/b0/26/e7b0269da7c8635849a9a6b15a5c2fb3.jpg" alt="">
+  <iframe width="560" height="315" src="https://www.youtube.com/embed/f6VarMoS4kI" frameborder="0" allowfullscreen></iframe>
 </body>
 </html>
